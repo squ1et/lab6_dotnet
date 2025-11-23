f26da45ff9fb186e0d072b9c493e51294e753d4a
squ1et <squ14sss@gmail.com>
2025-11-23 17:47:22 +0200

    changed_tests

--- lab6_test/Test1.cs
+++ lab6_test/Test1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using lab6_dotnet;
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace lab6_test
 {
@@ -36,6 +38,48 @@ namespace lab6_test
             list.SortByMileage();
 
             Assert.AreEqual(100, list.Cars[0].Mileage);
+        }
+
+        [TestMethod]
+        public void SaveToFile_And_LoadFromFile_WorkCorrectly()
+        {
+            string tempFile = Path.GetTempFileName();
+
+            try
+            {
+                CarList listToSave = new CarList();
+                listToSave.Add(new Car("Toyota", "Ivanov", 2015, 50000));
+                listToSave.Add(new Car("Ford", "Petrenko", 2018, 30000));
+
+                listToSave.SaveToFile(tempFile);
+
+                CarList listLoaded = new CarList();
+                listLoaded.LoadFromFile(tempFile);
+
+                Assert.AreEqual(listToSave.Cars.Count, listLoaded.Cars.Count);
+
+                for (int i = 0; i < listToSave.Cars.Count; i++)
+                {
+                    Assert.AreEqual(listToSave.Cars[i].Brand, listLoaded.Cars[i].Brand);
+                    Assert.AreEqual(listToSave.Cars[i].OwnerSurname, listLoaded.Cars[i].OwnerSurname);
+                    Assert.AreEqual(listToSave.Cars[i].Year, listLoaded.Cars[i].Year);
+                    Assert.AreEqual(listToSave.Cars[i].Mileage, listLoaded.Cars[i].Mileage);
+                }
+            }
+            finally
+            {
+                if (File.Exists(tempFile))
+                    File.Delete(tempFile);
+            }
+        }
+
+        [TestMethod]
+        public void LoadFromFile_InvalidPath_ThrowsException()
+        {
+            CarList list = new CarList();
+
+            Assert.ThrowsException<FileNotFoundException>(() =>
+                list.LoadFromFile("non_existing_file.txt"));
         }
     }
 }
